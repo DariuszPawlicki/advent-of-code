a0758092089093f9a86d173f6ad7f067a7882e7a
Dariusz Pawlicki <darekpl9gmail.com>
2023-02-12 14:21:48 +0100

    finished first day tasks

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,10 +1,10 @@
 #include <string> 
 #include <vector>
 #include <ranges>
+#include <format>
 #include <fstream>
 #include <numeric>
 #include <iostream>
-#include <algorithm>
 
 
 std::vector<std::vector<int>> parseData(const std::string& file_path) {
@@ -14,7 +14,6 @@ std::vector<std::vector<int>> parseData(const std::string& file_path) {
 	std::ifstream data{ file_path };
 
 	std::string line;
-
 	while (std::getline(data, line)) {
 		if (line != "") {
 			elf_calories.push_back(std::stoi(line));
@@ -30,19 +29,42 @@ std::vector<std::vector<int>> parseData(const std::string& file_path) {
 	return all_elves_calories;
 }
 
-int findMaxCalories(std::vector<std::vector<int>> all_elves_calories)
+// Part 1
+
+void printMaxCalories(std::vector<std::vector<int>> all_elves_calories)
 {
-	return std::ranges::max(
+	std::cout << std::format("Max calories: {}\n", std::ranges::max(
 		all_elves_calories | std::views::transform([](auto& cals) {
 			return std::accumulate(cals.begin(), cals.end(), 0);
 			})
-	);
+	));
+}
+
+// Part 2
+
+void printFirstNMaxCalories(size_t n, const std::vector<std::vector<int>>& all_elves_calories)
+{
+	std::vector<int> summed_calories(all_elves_calories.size());
+
+	std::ranges::transform(all_elves_calories, summed_calories.begin(), [](auto& cals) {
+		return std::accumulate(cals.begin(), cals.end(), 0);
+		});
+
+	std::ranges::sort(summed_calories, std::greater());
+
+	int sum{ 0 };
+	for (auto cals : summed_calories | std::views::take(3))
+		sum += cals;
+
+	std::cout << std::format("Top 3 calories sum: {}\n", sum);
 }
 
 int main() {
 	auto all_elves_calories = parseData("./data.txt");
 
-	std::cout << findMaxCalories(all_elves_calories) << std::endl;
+	printMaxCalories(all_elves_calories);
+
+	printFirstNMaxCalories(3, all_elves_calories);
 
 	return 0;
 }
