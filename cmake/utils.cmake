5e1686f92073cbbdec85a97f1df0adea307fb0b5
Dariusz Pawlicki <darekpl9gmail.com>
2023-03-14 17:39:06 +0100

    added GTest to CMake

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,5 +1,26 @@
+include(FetchContent)
+
 function(create_task_executable TARGET_NAME SOURCE_FILE)
 	add_executable(${TARGET_NAME} ${SOURCE_FILE})
 
 	file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/data.txt DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+endfunction()
+
+
+function(create_test TEST_NAME SOURCE_FILE)
+	add_executable(${TEST_NAME} ${SOURCE_FILE})
+
+	target_link_libraries(${TEST_NAME} PRIVATE gtest_main)
+
+	add_test(${TEST_NAME} ${TEST_NAME})
+endfunction()
+
+function(setup_gtest)
+	FetchContent_Declare(
+		googletest
+		GIT_REPOSITORY https://github.com/google/googletest.git
+		GIT_TAG release-1.11.0
+	)
+
+	FetchContent_MakeAvailable(googletest)
 endfunction()
